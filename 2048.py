da6e87c16c6526e2790ab0be0013f23dbb671159
Chinmay Bansal <chinmaybansalsc@outlook.com>
2022-04-24 21:55:53 -0400

    Create colors.py and 2048.py

---
+++ 2048.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__()
+        self.grid()
+        self.master.title("2048")        
+
+
+
+
+
+
+
+
+
+
